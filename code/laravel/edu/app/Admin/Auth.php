5034a63eab6520a8b13721ee03bab8a589a3f188
aoding9 <xy5217733@163.com>
2021-03-10 15:06:53 +0800

    对权限树方法进行注释,以免以后看忘记了

--- code/laravel/edu/app/Admin/Auth.php
+++ code/laravel/edu/app/Admin/Auth.php
@@ -46,25 +46,29 @@ class Auth extends Model
   //   }]);
   // }
 
-  // 
+  // 递归 返回权限树
   static public function tree($auths, $pid = 0, $tree = [])
   {
-    // 0级权限为顶级
+    // 记录当前递归的层级,也就是权限层级,0级权限为顶级权限的父级
     static $level = 1;
+    // 循环每个row
     foreach ($auths as $key => $value) {
+      // 第一层,如果pid!=0,说明不是顶级权限,直接跳过(减少重复递归)
+      // 第二层,如果pid!=顶级权限id,说明不是二级权限,直接跳过 ...
       if ($value['attributes']['pid'] == $pid) {
-        $tree[$key] =  (Object)$value['attributes'];
-        $tree[$key]->level =  $level;
+        $tree[$key] =  (Object)$value['attributes']; // 转成对象形式,方便在模板里面使用
+        $tree[$key]->level =  $level; // 记录当前层级
+        // 调用上面定义的child方法判断有没有子级
         if (!empty($value->child)) {
-          $level++;
-          $tree[$key]->child =  self::tree($value->child, $value['attributes']['id']);
+          $level++;  // 有子级,那么下一次递归的层级+1
+          $tree[$key]->child =  self::tree($value->child, $value['attributes']['id']); // 追加child属性,然后对child属性的赋值进行递归(将子级权限作为下一次递归的循环项,id作为新的父级id)
         } else {
-          $level=1;
-          return $value['attributes'];
+          $level=1;  // 没有子级,说明一个顶级权限分支递归到底了,重置为1级权限,
+          return $value['attributes']; //  返回底层权限row的字段列表,向上回归到child赋值,然后结束一轮顶层循环,继续递归下一个顶级权限
         }
       }
     }
-    return $tree;
+    return $tree; // 递归完成,返回整个tree
   }
 
 
